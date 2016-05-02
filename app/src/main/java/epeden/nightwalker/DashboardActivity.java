09dc3884ebd04dab75206ce99c1e488fe94c1057
epeden <ericmpeden@gmail.com>
2016-05-02 15:52:40 -0400

    working on timelineView

--- app/src/main/java/epeden/nightwalker/DashboardActivity.java
+++ app/src/main/java/epeden/nightwalker/DashboardActivity.java
@@ -43,11 +43,10 @@ public class DashboardActivity extends AppCompatActivity {
         myDataset[0] = "WASSUP";
         myDataset[1] = "AGAIN";
 
-        // specify an adapter (see also next example)
 
 
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
+//        mAdapter = new MyAdapter(myDataset);
+//        mRecyclerView.setAdapter(mAdapter);
 
     }
 
@@ -58,53 +57,53 @@ public class DashboardActivity extends AppCompatActivity {
 
 
 
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        private String[] mDataset;
-
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            // each data item is just a string in this case
-            public View mView;
-            public TextView mTextView;
-            public ViewHolder(View v) {
-                super(v);
-                mView = v;
-                mTextView = (TextView) findViewById(R.id.info_text);
-            }
-        }
-
-        // Provide a suitable constructor (depends on the kind of dataset)
-        public MyAdapter(String[] myDataset) {
-            mDataset = myDataset;
-
-        }
-
-        // Create new views (invoked by the layout manager)
-        @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            // create a new view
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.card_layout, parent, false);
-            // set the view's size, margins, paddings and layout parameters
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            // - get element from your dataset at this position
-            // - replace the contents of the view with that element
-            holder.mTextView.setText(mDataset[position]);
-
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
-        @Override
-        public int getItemCount() {
-            return mDataset.length;
-        }
-    }
+//    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+//        private String[] mDataset;
+//
+//        // Provide a reference to the views for each data item
+//        // Complex data items may need more than one view per item, and
+//        // you provide access to all the views for a data item in a view holder
+//        public class ViewHolder extends RecyclerView.ViewHolder {
+//            // each data item is just a string in this case
+//            public View mView;
+//            public TextView mTextView;
+//            public ViewHolder(View v) {
+//                super(v);
+//                mView = v;
+//                mTextView = (TextView) findViewById(R.id.info_text);
+//            }
+//        }
+//
+//        // Provide a suitable constructor (depends on the kind of dataset)
+//        public MyAdapter(String[] myDataset) {
+//            mDataset = myDataset;
+//
+//        }
+//
+//        // Create new views (invoked by the layout manager)
+//        @Override
+//        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            // create a new view
+//            View v = LayoutInflater.from(parent.getContext())
+//                    .inflate(R.layout.card_layout, parent, false);
+//            // set the view's size, margins, paddings and layout parameters
+//            ViewHolder vh = new ViewHolder(v);
+//            return vh;
+//        }
+//
+//        // Replace the contents of a view (invoked by the layout manager)
+//        @Override
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//            // - get element from your dataset at this position
+//            // - replace the contents of the view with that element
+//            holder.mTextView.setText(mDataset[position]);
+//
+//        }
+//
+//        // Return the size of your dataset (invoked by the layout manager)
+//        @Override
+//        public int getItemCount() {
+//            return mDataset.length;
+//        }
+//    }
 }
