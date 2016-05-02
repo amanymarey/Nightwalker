39f9e0e5ae29c365da25dfab2a1e5cd8c9409fbb
andriyrusyn <rusyn.andriy@gmail.com>
2016-05-01 23:50:36 -0400

    commented out toasts

--- app/src/main/java/epeden/nightwalker/SettingsActivity.java
+++ app/src/main/java/epeden/nightwalker/SettingsActivity.java
@@ -35,18 +35,18 @@ public class SettingsActivity extends AppCompatActivity {
                     newSnoozeLimit = progresValue;
                     settings.setSnoozeLimit(newSnoozeLimit);
                     settings.save();
-                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onStartTrackingTouch(SeekBar seekBar) {
-                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
                     snoozeLimitTextView.setText("Covered: " + newSnoozeLimit + "/" + seekBar.getMax());
-                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
                 }
 
             });
@@ -60,18 +60,18 @@ public class SettingsActivity extends AppCompatActivity {
                     newSnoozeDuration = progresValue;
                     settings.setSnoozeDuration(newSnoozeDuration);
                     settings.save();
-                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onStartTrackingTouch(SeekBar seekBar) {
-                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
-                    snoozeLimitTextView.setText("Covered: " + newSnoozeDuration + "/" + seekBar.getMax());
-                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+                    snoozeDurationTextView.setText("Snooze Duration: " + newSnoozeDuration + "/" + seekBar.getMax());
+//                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
                 }
 
             });
