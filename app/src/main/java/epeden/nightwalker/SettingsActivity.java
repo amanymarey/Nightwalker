6f87a9cbf3758a166e89c2fde04dd4b85cbb1bb9
epeden <ericmpeden@gmail.com>
2016-05-05 03:10:56 -0400

    added range slider view

--- app/src/main/java/epeden/nightwalker/SettingsActivity.java
+++ app/src/main/java/epeden/nightwalker/SettingsActivity.java
@@ -8,6 +8,8 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.channguyen.rsv.RangeSliderView;
+
 import org.w3c.dom.Text;
 
 /**
@@ -19,62 +21,105 @@ public class SettingsActivity extends AppCompatActivity {
         SeekBar snoozeDurationSeekBar;
         TextView snoozeDurationTextView;
 
-        Settings settings = Settings.findById(Settings.class, 1);
+        private RangeSliderView snoozeLimitSlider;
+    private RangeSliderView snoozeDurationSlider;
+
+
+    Settings settings = Settings.findById(Settings.class, 1);
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_settings);
-            snoozeLimitTextView = (TextView) findViewById(R.id.snoozeLimitTextView);
-            snoozeLimitSeekBar = (SeekBar) findViewById(R.id.snoozeLimitSeekBar);
-            snoozeLimitSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                int newSnoozeLimit = 0;
 
+
+
+            snoozeLimitTextView = (TextView) findViewById(R.id.snoozeLimitTextView);
+            snoozeDurationTextView = (TextView) findViewById(R.id.snoozeDurationTextView);
+
+            snoozeLimitSlider = (RangeSliderView) findViewById(R.id.snoozeLimitRangeSlider);
+            snoozeDurationSlider = (RangeSliderView) findViewById(R.id.snoozeDurationRangeSlider);
+
+            final RangeSliderView.OnSlideListener limitListener = new RangeSliderView.OnSlideListener() {
                 @Override
-                public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
-                    newSnoozeLimit = progressValue;
+                public void onSlide(int index) {
+                    int newSnoozeLimit = 0;
+                    newSnoozeLimit = index;
                     settings.setSnoozeLimit(newSnoozeLimit);
                     settings.save();
-//                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+                    snoozeLimitTextView.setText("Covered: " + newSnoozeLimit + "/" + snoozeLimitSlider.getRangeCount());
                 }
-
+            };
+            final RangeSliderView.OnSlideListener durationListener = new RangeSliderView.OnSlideListener() {
                 @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-//                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                    snoozeLimitTextView.setText("Covered: " + newSnoozeLimit + "/" + seekBar.getMax());
-//                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
-                }
-
-            });
-            snoozeDurationTextView = (TextView) findViewById(R.id.snoozeDurationTextView);
-            snoozeDurationSeekBar = (SeekBar) findViewById(R.id.snoozeDurationSeekBar);
-            snoozeDurationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                int newSnoozeDuration = 0;
-
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
-                    newSnoozeDuration = progresValue;
+                public void onSlide(int index) {
+                    int newSnoozeDuration = 0;
+                    newSnoozeDuration = index;
                     settings.setSnoozeDuration(newSnoozeDuration);
                     settings.save();
-//                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+                    snoozeDurationTextView.setText("Snooze Duration: " + newSnoozeDuration + "/" + snoozeDurationSlider.getRangeCount());
                 }
+            };
 
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-//                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
-                }
+            snoozeLimitSlider.setOnSlideListener(limitListener);
+            snoozeDurationSlider.setOnSlideListener(durationListener);
 
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                    snoozeDurationTextView.setText("Snooze Duration: " + newSnoozeDuration + "/" + seekBar.getMax());
-//                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
-                }
 
-            });
+
+
+
+
+
+//            snoozeLimitTextView = (TextView) findViewById(R.id.snoozeLimitTextView);
+//            snoozeLimitSeekBar = (SeekBar) findViewById(R.id.snoozeLimitSeekBar);
+//            snoozeLimitSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//                int newSnoozeLimit = 0;
+//
+//                @Override
+//                public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+//                    newSnoozeLimit = progressValue;
+//                    settings.setSnoozeLimit(newSnoozeLimit);
+//                    settings.save();
+////                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onStartTrackingTouch(SeekBar seekBar) {
+////                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onStopTrackingTouch(SeekBar seekBar) {
+//                    snoozeLimitTextView.setText("Covered: " + newSnoozeLimit + "/" + seekBar.getMax());
+////                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+//                }
+//
+//            });
+//            snoozeDurationTextView = (TextView) findViewById(R.id.snoozeDurationTextView);
+//            snoozeDurationSeekBar = (SeekBar) findViewById(R.id.snoozeDurationSeekBar);
+//            snoozeDurationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//                int newSnoozeDuration = 0;
+//
+//                @Override
+//                public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+//                    newSnoozeDuration = progresValue;
+//                    settings.setSnoozeDuration(newSnoozeDuration);
+//                    settings.save();
+////                    Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onStartTrackingTouch(SeekBar seekBar) {
+////                    Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onStopTrackingTouch(SeekBar seekBar) {
+//                    snoozeDurationTextView.setText("Snooze Duration: " + newSnoozeDuration + "/" + seekBar.getMax());
+////                    Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+//                }
+//
+//            });
         }
 
     public void backButtonPushed(View v) {
